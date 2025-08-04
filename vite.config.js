9db1ce3e3b3f0bae16b76c0772ad5e9df3750631
RSGSYS\NeerajKu <neerajkumar@rsgmedia.com>
2025-08-04 10:13:05 +0530

    changes to deploy it on gitgub pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/pheraz/', // replace REPO_NAME with your actual GitHub repo name
 })
