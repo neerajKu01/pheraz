ac1ac3f62e0a18d989ce73c4fa6e7b2d42e681bb
RSGSYS\NeerajKu <neerajkumar@rsgmedia.com>
2025-08-04 10:31:20 +0530

    git pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/pheraz/', // replace REPO_NAME with your actual GitHub repo name
-})
+  base: '/pheraz/',
+  plugins: [react()]
+});
